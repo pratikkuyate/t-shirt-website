32db6fb0d21fb3a654d892fb6d5da527ec148b87
pratikkuyate <94629983+pratikkuyate@users.noreply.github.com>
2023-12-12 16:21:27 +0530

    Update Home.jsx comment logo section in home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,7 +8,6 @@ import {
   headContentAnimation,
   slideAnimation,
 } from "../src/config/config/motion";
-import Image from "next/image";
 import { CustomButton } from "../src/components";
 
 const Hero = () => {
@@ -17,15 +16,15 @@ const Hero = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
-          <motion.header {...slideAnimation("down")}>
-            <img
+          {/* <motion.header {...slideAnimation("down")}> */}
+            {/* <img
               src="../public/threejs.png"
               alt="threejs"
               className="w-8 h-8 object-contain"
               width={100}
               height={100}
-            />
-          </motion.header>
+            /> */}
+          {/* </motion.header> */}
           <motion.div className="home-container" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
@@ -34,7 +33,7 @@ const Hero = () => {
               </h1>
             </motion.div>
             <motion.div>
-              <p className="max-w-md font-normal text-gray-600 text-base">
+              <p className="max-w-md mb-6 font-normal text-gray-600 text-lg">
                 Create your unique and exclusive shirt with our new 3D customization tool.{" "}
                 <strong>Unleash your imagination</strong> and define your own style.
               </p>
